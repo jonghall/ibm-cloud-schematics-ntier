d2889c6e96ff932431d3e461c7abec67e9fe552f
Jon Hall <jon@hallusa.com>
2019-11-12 09:33:54 -0600

    initial

--- cloud-init-client.tf
+++ cloud-init-client.tf
@@ -14,12 +14,12 @@ data "template_cloudinit_config" "cloud-init-client" {
   part {
     filename      = "init.cfg"
     content_type  = "text/cloud-config"
-    content       = "${data.local_file.cloud-init-text.content_base64}"
+    content       = "${data.local_file.cloud-init-text.content}"
   }
 
   part {
     filename      = "RaxakProtectSetup.sh"
     content_type  = "text/x-shellscript"
-    content       =  "${data.local_file.script.content_base64}"
+    content       =  "${data.local_file.script.content}"
   }
 }
