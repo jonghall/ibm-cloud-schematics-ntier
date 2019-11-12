5d6623f24ae3a5c3e0b6bcbba453327c23e65fe7
Jon Hall <jon@hallusa.com>
2019-11-12 09:23:17 -0600

    initial

--- cloud-init-client.tf
+++ cloud-init-client.tf
@@ -1,25 +1,25 @@
 
-data "template_file" "cloud-init-config" {
-  template = "${file("cloud-init.txt")}"
+data "local_file" "cloud-init-text" {
+  filename        = "cloud-init.txt"
 }
 
-data "template_file" "script" {
-  template = "${file("RaxakProtectSetup.sh")}"
+data "local_file" "script" {
+  filename        = "RaxakProtectSetup.sh"
 }
 
 data "template_cloudinit_config" "cloud-init-client" {
-  base64_encode = true
-  gzip          = true
+  base64_encode   = true
+  gzip            = true
 
   part {
     filename      = "init.cfg"
     content_type  = "text/cloud-config"
-    content       = "${data.template_file.cloud-init-config.rendered}"
+    content       = "${data.local_file.cloud-init-text.content_base64}"
   }
 
   part {
     filename      = "RaxakProtectSetup.sh"
-    content_type  = "text/shellscript"
-    content       =  "${data.template_file.script.rendered}"
+    content_type  = "text/x-shellscript"
+    content       =  "${data.local_file.script.content_base64}"
   }
 }
