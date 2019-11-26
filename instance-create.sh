86c4bc7e33647d57f596d45b34b0b9365242f0f0
Jon Hall <jon@hallusa.com>
2019-11-26 15:03:31 -0600

    initial

--- instance-create.sh
+++ instance-create.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 vsiname=$1
-vpc="7b4bd245-37eb-456a-b5e2-458b43ddb98c"
-subnet="0ea2283c-f545-44a5-ad2b-d2c5343bc214"
+vpc="r006-6b62c36a-6375-4c6b-80c9-65fd03ce182d "
+subnet="0717-bcf430b6-4fad-4a4a-96a3-0a5cd90f9a6f "
 region="us-south-1"
-profile="cc1-2x4"
-image="cc8debe0-1b30-6e37-2e13-744bfb2a0c11"
-securitygroup="2d364f0a-a870-42c3-a554-000002178570"
-key="636f6d70-0000-0001-0000-000000170bf4"
+profile="cx2-2x4"
+image="99edcc54-c513-4d46-9f5b-36243a1e50e2"
+securitygroup="r006-0815632a-34f1-484c-9fb7-c9bbe6afae85"
+key="r006-870b4d7e-02dd-47c3-a1b8-733504a6b98a"
 
-NICID=$(ibmcloud is instance-create $vsiname $vpc $region cc1-2x4 $subnet --key-ids $key --image-id $image --security-group-ids $securitygroup --user-data @RaxakProtectSetup.sh | awk '/Primary interface/ {print $3}' | sed 's/.*(\(.*\))/\1/')
+ibmcloud is target --gen 2
+#ibmcloud is instance-create $vsiname $vpc $region $profile $subnet --key-ids $key --image-id $image --security-group-ids $securitygroup --user-data @RaxakProtectSetup.sh
+NICID=$(ibmcloud is instance-create $vsiname $vpc $region $profile $subnet --key-ids $key --image-id $image --security-group-ids $securitygroup --user-data @RaxakProtectSetup.sh | awk '/Primary interface/ {print $3}' | sed 's/.*(\(.*\))/\1/')
 sleep 5
 ibmcloud is floating-ip-reserve $vsiname-fip --nic-id $NICID
